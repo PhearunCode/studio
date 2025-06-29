3ea4ae0ca00f54c5be32da3abdd7e9ad119ee687
Phearun Yano <phearunyano@gmail.com>
2025-06-29 14:01:30 +0000

    Connect firebase

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
-  },
+  serverExternalPackages: ['firebase-admin'],
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
