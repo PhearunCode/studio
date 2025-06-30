dbfbc64d7cc478b4c72ed744e8df2c39de6213cc
Phearun Yano <phearunyano@gmail.com>
2025-06-30 00:08:00 +0000

    Connect firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,6 +20,7 @@ if (!getApps().length) {
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
       });
+      console.log('Firebase Admin SDK initialized successfully.');
     } catch (error) {
       console.error('Firebase admin initialization error:', error);
     }
